00d5be891007874b10ade7edc02b68695f6c3f8a
JamesKang003 <kangjames5@gmail.com>
2023-06-06 16:18:44 +0900

    what is JSX?

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
-
-/* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
-
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
-*/
-
 const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program";
+h1.textContent = "Hello world";
 h1.className = "header";
-document.getElementById("root").append(h1);
+console.log(h1);
+
+// <h1 class="header">
+
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+
+/*
+{
+    type: "h1", 
+    key: null, 
+    ref: null, 
+    props: {
+        className: "header", 
+        children: "This is JSX"
+    }, 
+    _owner: null, 
+    _store: {}
+}
+ */
+
+// JSX
+ReactDOM.render(element, document.getElementById("root"));

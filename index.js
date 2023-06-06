c61d342a7e6bbceef099b9f8581cf372989ae941
JamesKang003 <kangjames5@gmail.com>
2023-06-06 15:54:30 +0900

    lists added to html using javascript

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+ReactDOM.render(
+  <ul>
+    <li>1st item</li>
+    <li>2nd item</li>
+  </ul>,
+  document.getElementById("root")
+);

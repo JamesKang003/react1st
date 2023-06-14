298e1f56591314a1fba910c2fcc1317409638fa9
JamesKang003 <kangjames5@gmail.com>
2023-06-14 21:48:01 +0900

    style change

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
-import data from "./data"
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
 
 export default function App() {
-    const cards = data.map(item => {
-        return (
-            <Card
-                key={item.id}
-                {...item}
-                
-            />
-        )
-    })        
-    
-            // <Hero />
-    return (
-        <div>
-            <Navbar />
-            <section className="cards-list">
-                {cards}
-            </section>
-        </div>
-    )
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  // <Hero />
+  return (
+    <div>
+      <Navbar />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
 }
